7231af73848a22ba3e182c6aa41570fbcc8b9cc0
agustined <agustin_estevez_d@hotmail.com>
2024-02-13 17:59:25 +0000

    Subida archivos

---
+++ Capítulo 3/modificar_lista.py
@@ -0,0 +1,8 @@
+motos = ["Honda", "Piagio", "Suzuki"]
+print(motos)
+#Modificamos el primer elemento de la lista
+motos[0]="Ducatti"
+print(motos)
+#Añadimos un nuevo elemento a la lista
+motos.append("Honda")
+print(motos)
