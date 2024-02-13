23528ce2f6f36239ecd3d130558d4a8cabd7b6a8
agustined <agustin_estevez_d@hotmail.com>
2024-02-13 17:41:08 +0000

    Ejercicios capítulo 3

---
+++ Capítulo 3/listas_1.py
@@ -0,0 +1,8 @@
+bicicletas = ["trek", "cannondale", "specialized", "redline"]
+n=0
+
+while (n!=len(bicicletas)):
+    print(bicicletas[n].title())
+    n=n+1
+
+print("Fin de la lista.")
---
+++ Capítulo 3/listas_2.py
@@ -0,0 +1,6 @@
+amigos = ["josé", "nuria", "cristobal", "golem", "jonás", "paloma"]
+
+print(f"Mis amigos de Vigo se llaman {amigos[0].title()} y {amigos[1].title()}.")
+print(f"Mi amigo {amigos[2].title()} viaja a Sudamérica por trabajo.")
+print(f"Mis amigos {amigos[3].title()} y {amigos[4].title()} son uña y carne.")
+print(f"Mi amiga {amigos[-1].title()} vive en Madrid y no nos vemos mucho.")
