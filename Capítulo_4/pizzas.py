bd248fc17e972c8597630b5a6138a10fa9f4e8b6
agustined <agustin_estevez_d@hotmail.com>
2024-02-15 16:23:07 +0000

    Copiado de listas

--- Capítulo_4/pizzas.py
+++ Capítulo_4/pizzas.py
@@ -1,7 +1,15 @@
 pizzas = ["peperoni", "jamón y bacon", "cuatro quesos", "jamón y champiñones"]
+friend_pizzas = pizzas[-1:]
+
+pizzas.append("carbonara")
+friend_pizzas.append("alcachofas")
 
 print("Me gustan mucho las pizzas...")
 for pizza in pizzas:
     print(f"Me gusta la pizza de {pizza}.")
 
 print("Me encanta la pizza!")
+
+print("A mi amigo le gustan tambien...")
+for friend_pizza in friend_pizzas:
+    print(f"Le gusta la pizza de {friend_pizza}.")
