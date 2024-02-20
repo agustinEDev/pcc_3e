1dc4db1ec3dc3e8402f0e283753a8330b7213204
agustined <agustin_estevez_d@hotmail.com>
2024-02-20 23:40:03 +0100

    Argumentos indefinidos

--- Capítulo_8/argumentos_indefinidos.py
+++ Capítulo_8/argumentos_indefinidos.py
@@ -1,5 +1,6 @@
 def imprimir_nombres(*name):
-    print(f" - {name}")
+    for value in name:
+        print(f" - {value}")
 
 imprimir_nombres('Juan','Pedro','José','Ana','Luis')
 imprimir_nombres('Josesiño')
