7975ff130012d4bbb37254121fd7cfeba478fe00
agustined <agustin_estevez_d@hotmail.com>
2024-02-21 00:31:21 +0100

    import módulo

--- Capítulo_8/primera_funcion.py
+++ Capítulo_8/primera_funcion.py
@@ -1,8 +1,11 @@
-
+import switch
 def greet_user(usuario):
     print(f"Hola {usuario.title()}")
 
-#pedimos un nombre por pantalla y lo almacenamos en una variable.
-name = input("Introduce tu nombre: ")
-#Llamamos a la función creada anteriormente
-greet_user(name)
+flag = True
+while flag:
+    #pedimos un nombre por pantalla y lo almacenamos en una variable.
+    name = input("Introduce tu nombre: ")
+    #Llamamos a la función creada anteriormente
+    greet_user(name)
+    flag = switch.set_flag_es()
