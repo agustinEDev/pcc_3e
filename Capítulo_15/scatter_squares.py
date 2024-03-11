ee41d3edac356a9c124c87ccc71542c1bb10848e
agustined <agustin_estevez_d@hotmail.com>
2024-03-11 23:09:57 +0100

    Matplotlib

--- Capítulo_15/scatter_squares.py
+++ Capítulo_15/scatter_squares.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
 
-
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
-
-"""Muestra los estilos disponibles para el gráfico"""
-#print(plt.style.available)
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth = 3)
+ax.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Blues, s=10)
 
 #Establece el título del gráfico y las etiquetas de los ejes.
 ax.set_title("Square Numbers", fontsize = 24)
@@ -19,4 +15,9 @@ ax.set_ylabel("Square of Value", fontsize = 14)
 #Establece el tamaño de las etiquetas de los puntos de los ejes.
 ax.tick_params(labelsize = 14)
 
+#Establece el rango para cada eje.
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
