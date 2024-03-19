ae82e50cbcb6707fe679d89dd1763dae4831a523
agustinED <agustin_estevez_d@hotmail.com>
2024-03-19 19:38:40 +0100

    Capítulo 17

---
+++ Capítulo_17/hn_article.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+#Hace una llamada a una API y guarda la respuesta.
+url = "https://hacker-news.firebaseio.com/v0/item/31353677.json"
+r = requests.get(url)
+print(f"Status Code: {r.status_code}")
+
+#Explora la estructura de los datos.
+response_dict = r.json()
+response_string = json.dumps(response_dict, indent = 4)
+print(response_string)
