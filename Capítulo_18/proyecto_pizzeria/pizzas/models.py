7b53c4aff82a4ce3ab0ce7d11e4167ad4c3648d8
agustinEDev <agustin_estevez_d@hotmail.com>
2024-03-23 19:31:35 +0100

    Fin capítulo 18

--- Capítulo_18/proyecto_pizzeria/pizzas/models.py
+++ Capítulo_18/proyecto_pizzeria/pizzas/models.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Pizza(models.Model):
+class Pizza (models.Model):
     """Una pizza que el usuario puede pedir."""
     name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(default  = timezone.now)
 
     def __str__(self):
         """Devuelve una representación del modelo como cadena."""
@@ -13,6 +16,7 @@ class Ingrediente (models.Model):
     """Ingredientes para las pizzas."""
     pizza = models.ForeignKey(Pizza, on_delete = models.CASCADE)
     name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(default  = timezone.now)
 
     def __str__(self):
         """Devuelve una representación del modelo como cadena."""
--- Capítulo_18/proyecto_pizzeria/pizzas/urls.py
+++ Capítulo_18/proyecto_pizzeria/pizzas/urls.py
@@ -7,4 +7,8 @@ app_name = 'pizzas'
 urlpatterns = [
     # Página de inicio
     path('', views.index, name='index'),
+    # Página que muestra todas las pizzas
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # Página de detalles para una pizza individual
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
